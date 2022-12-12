929919aafc3dab4429f73bbaa8437013129e8a85
Who-codes <sulavshrestha082@gmail.com>
2022-12-12 13:09:29 +0545

    react router installed and added to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import Favourites from "./pages/Favourites";
+import NewMeetups from "./pages/NewMeetups";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<AllMeetups />} />
+      <Route path="/new-meetup" element={<NewMeetups />} />
+      <Route path="/favourites" element={<Favourites />} />
+    </Routes>
+  );
 };
 
 export default App;
