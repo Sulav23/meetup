973669372b4836e455e74e8a69ad6dbc9d2d281d
Who-codes <sulavshrestha082@gmail.com>
2022-12-12 13:57:54 +0545

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetups from "./pages/AllMeetups";
 import Favourites from "./pages/Favourites";
 import NewMeetups from "./pages/NewMeetups";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<AllMeetups />} />
-      <Route path="/new-meetup" element={<NewMeetups />} />
-      <Route path="/favourites" element={<Favourites />} />
-    </Routes>
+    <>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetups />} />
+        <Route path="/new-meetup" element={<NewMeetups />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </>
   );
 };
 
